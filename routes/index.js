7836425c87edc963bba1d9fc290b2243a1f4a873
kenps rossi <kenpsrossi@gmail.com>
2022-12-02 22:01:37 -0300

    api fake json seg parte

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const clientesController = require('../controllers/clientesController');
 const router = express.Router();
 const HomeController = require("../controllers/homeController")
 
 router.get('/', HomeController.index);
+
+router.get('/clientes', clientesController.index);
 
 module.exports = router;
